2be1c6b3cbe2ff1325f5e16788ddbd0972e26fb8
Alex Hazoury <a.hazoury@gmail.com>
2013-03-27 11:55:53 +0000

    refactored to show proper Login and Logout links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in!'
+      redirect_to root_path
     else
-      flash.now.alert = 'Email or password is invalid!'
-      redirect_to new_session_path
+      redirect_to new_session_path, notice: 'Email or password is invalid!'
     end
   end
 
